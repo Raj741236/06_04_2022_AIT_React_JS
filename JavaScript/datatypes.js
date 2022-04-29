1ea760199a4bc0d28000fc1ef9351f7f5456c1e2
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-04-29 08:04:54 +0530

    datatypes in js

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,22 @@
+const firstName = 'John';
+console.log(typeof firstName);
+
+//Number: 
+
+let age = 26;
+console.log(typeof age);
+age = "twenty six";
+console.log(typeof age);
+
+//Boolean:true/false it is used for making decisions
+
+const isJavascriptEasy = true;
+console.log(typeof isJavascriptEasy);
+
+//undefined:when you declare a variable and if you are not assigning any value then JS will declare it as undefined
+
+let place=null;
+console.log(place);
+
+//null:it is nothing but empty value
+
---
+++ JavaScript/introduction_to_js.js
@@ -0,0 +1,19 @@
+var trainingInstitute =  "AchiversIt(BTM)";
+var trainingInstitute = "AchiversIT(Marathalli)"
+//    var trainingInstitute;
+//    trainingInstitute = "AchiversIT";
+console.log(trainingInstitute);
+ 
+let firstName = "Rajashekar";
+firstName = "John";
+// let firstName;
+// firstName = "Rajashekar";
+console.log(firstName);
+
+const age=26;
+
+
+console.log(age)
+
+const person = "rajesh";
+console.log(person);
