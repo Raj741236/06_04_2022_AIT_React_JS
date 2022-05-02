17cb17fd5daa552d753fb2315a8b26bf74f1945d
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-05-02 07:05:37 +0530

    operators in js

---
+++ JavaScript/operators.js
@@ -0,0 +1,45 @@
+//Arithmetic operators
+
+const currentYear = 2045;
+const ageOfRam = currentYear - 1991;
+const ageOfShyam = currentYear - 2001;
+
+console.log(ageOfRam,ageOfShyam);
+
+console.log(10%2);
+
+console.log(2**4)//2 to the power 3 or 2 * 2* 2
+
+//
+
+let x = 5 + 6;
+x *= 8;//x= x*8;
+
+ console.log(x)
+//increment and decrement operators
+ x++;//x=x+1;
+ x--;//x=x-1;
+ x--;//x=x-1
+ console.log(x);
+
+ //==: It will check that both the values are same it will ignore datatype
+
+ console.log(2 == '2');
+
+ //=== : It will check both the values as well as datatype
+
+ console.log(false == 0);
+
+
+
+  console.log((25 + 25)/2);
+
+  //truthy value and falsy values
+
+  console.log(Boolean(0));
+  console.log(Boolean(''));
+  console.log(Boolean('raj'));
+  console.log(Boolean({}));
+  console.log(Boolean(null));
+
+ 
