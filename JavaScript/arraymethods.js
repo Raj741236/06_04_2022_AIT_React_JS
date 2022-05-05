60e722f68e88cd179d77222b30823c43d1e9f446
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-05-05 08:34:59 +0530

    array methods in js

---
+++ JavaScript/arraymethods.js
@@ -0,0 +1,86 @@
+//length
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+
+// console.log(students.length);
+
+//push method
+//if you want to add elements at the ending of an array
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+
+// students.push("akash");
+// console.log(students);
+
+//pop method
+//if you want to remove element from the end
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+// students.pop();
+// students.pop();
+// console.log(students);
+
+//unshift method
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+// students.unshift("akash");
+// console.log(students);
+
+//shift method
+// const students = ["ram","shyam","john","alex","rajesh"];
+// console.log(students.shift());
+// console.log(students);
+
+//slice method
+//It is used to extract the portion of an array
+//If you apply the slice method will not distrub the original array
+//Syntax: 1. slice()
+//        2. slice(startindex)
+ //         3. slice(startindex,lastindex)
+
+//  const students = ["ram","shyam","john","alex","rajesh"];
+//   students.slice(1,4);
+//   console.log(students);
+// //   console.log(result);
+
+//splice method
+//It adds and/or removes array elements
+//It will distrub the original array
+//syntax: splice(start)
+  //        splice(start,deletecount)
+  //        splice(start,deletecount,iteem1......item(n))
+
+//   const students = ["ram","shyam","john","alex","rajesh"];
+
+//   const result  =students.splice("akash","sita")
+//   console.log(students);
+//   console.log(result);
+
+//includes methods
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+
+// console.log(students.includes("ramu"));
+
+//indexOf method
+// const students = ["ram","shyam","ram","john","alex","rajesh","ram"];
+// console.log(students.indexOf("ram",1));
+
+//lastindexof method
+// const students = ["ram","shyam","john","alex","rajesh"];
+// console.log(students.lastIndexOf("ram"));
+
+//reverse method
+
+// const students = ["ram","shyam","john","alex","rajesh"];
+// console.log(students.reverse());
+// console.log(students);
+
+//join method
+const students = ["ram","shyam","john","alex","rajesh"];
+console.log(students.join('/'));
+
+
+
+
+     
